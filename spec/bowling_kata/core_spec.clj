c621c270fead38349eb54d1c69475ab729380efa
Alex Jensen <alex@endure.io>
2023-04-18 17:53:27 -0400

    'The Fourth Test' passes

--- spec/bowling_kata/core_spec.clj
+++ spec/bowling_kata/core_spec.clj
@@ -9,6 +9,11 @@
       rolls
       (recur (inc n) (roll rolls pins)))))
 
+(defn roll-spare [rolls]
+  (-> rolls
+      (roll 5)
+      (roll 5)))
+
 (describe "Bowling scoring tests"
   (it "should score a game of gutter balls as 0"
     (should= 0 (score-game (roll-many 20 0))))
@@ -16,7 +21,12 @@
     (should= 20 (score-game (roll-many 20 1))))
   (it "should give the bonus for scoring a spare"
     (should= 16 (score-game (-> []
-                                (roll 5)
-                                (roll 5)
+                                (roll-spare)
                                 (roll 3)
-                                (concat (roll-many 17 0)))))))
+                                (concat (roll-many 17 0))))))
+  (it "should give the bonus for scoring a strike"
+    (should= 24 (score-game (-> []
+                                (roll 10)
+                                (roll 3)
+                                (roll 4)
+                                (concat (roll-many 16, 0)))))))
--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,7 +1,10 @@
 (ns bowling-kata.core)
 
 (defn roll [rolls pins]
-  (conj rolls pins))
+  (let [rolls (conj rolls pins)]
+    (if (= pins 10)
+      (conj rolls 0)
+      rolls)))
 
 (defn is-spare [rolls n]
   (= 10 (+ (nth rolls n) (nth rolls (+ n 1)))))
@@ -10,13 +13,17 @@
   (+ 10 (nth rolls (+ 2 n))))
 
 (defn score-frame [rolls n]
-  (if (is-spare rolls n)
-    (score-spare rolls n)
-    (+ (nth rolls n) (nth rolls (+ n 1)))))
+  (if (= 10 (nth rolls n))
+    (+ 10 (nth rolls (+ 2 n)) (nth rolls (+ 3 n)))
+    (if (is-spare rolls n)
+      (score-spare rolls n)
+      (+ (nth rolls n) (nth rolls (+ n 1))))))
 
 (defn score-game [rolls]
+  (print rolls "\n")
   (loop [n      0
          score  0]
+    (print n " - " score "\n")
     (if (>= n 20)
       score
       (recur (+ n 2) (+ score (score-frame rolls n))))))
