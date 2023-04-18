747e9b3171a99a4b3c3672f06d55e117589cf35b
Alex Jensen <alex@endure.io>
2023-04-18 15:40:38 -0400

    Refactored tests

--- spec/bowling_kata/core_spec.clj
+++ spec/bowling_kata/core_spec.clj
@@ -2,16 +2,15 @@
   (:require [speclj.core :refer :all]
             [bowling-kata.core :refer :all]))
 
+(defn roll-many [num-rolls pins]
+  (loop [n     0
+         rolls ()]
+    (if (>= n num-rolls)
+      rolls
+      (recur (inc n) (roll rolls pins)))))
+
 (describe "Bowling scoring tests"
   (it "should score a game of gutter balls as 0"
-    (should= 0 (score-game (loop [n     0
-                                  rolls ()]
-                             (if (>= n 20)
-                               rolls
-                               (recur (inc n) (roll rolls 0)))))))
+    (should= 0 (score-game (roll-many 20 0))))
   (it "should score a game of 1's as 20"
-    (should= 20 (score-game (loop [n      0
-                                   rolls  ()]
-                              (if (>= n 20)
-                                rolls
-                                (recur (inc n) (roll rolls 1))))))))
+    (should= 20 (score-game (roll-many 20 1)))))
