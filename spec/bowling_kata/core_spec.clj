06a49e305a85a18483510be30f196ccf08f183d6
Alex Jensen <alex@endure.io>
2023-04-19 15:11:52 -0400

    Scores games withot special rolls correctly

--- spec/bowling_kata/core_spec.clj
+++ spec/bowling_kata/core_spec.clj
@@ -7,4 +7,10 @@
     (should= [0] (roll '() 0)))
 
   (it "should add successful roll to a given game"
-    (should= [1] (roll '() 1))))
+    (should= [1] (roll '() 1)))
+
+  (it "should score gutter game as 0"
+    (should= 0 (score (nth (iterate #(roll % 0) '()) 20))))
+
+  (it "should score game of 1's as 20"
+    (should= 20 (score (nth (iterate #(roll % 1) '()) 20)))))
--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -2,3 +2,6 @@
 
 (defn roll [rolls pins]
   (conj rolls pins))
+
+(defn score [rolls]
+  (reduce + rolls))
