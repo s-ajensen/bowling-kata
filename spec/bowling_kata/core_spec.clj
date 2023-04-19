47309b2beb92c857b18713f246f24744a9ba36ad
Alex Jensen <alex@endure.io>
2023-04-19 16:09:41 -0400

    Refactored game scoring

--- spec/bowling_kata/core_spec.clj
+++ spec/bowling_kata/core_spec.clj
@@ -3,20 +3,27 @@
             [bowling-kata.core :refer :all]))
 
 (def new-game
-  '())
+  [])
 
 (defn roll-many [n pins]
   (nth (iterate #(roll % pins) new-game) n))
 
 (describe "Bowling game tests"
   (it "should add gutter roll to a given game"
-    (should= [0] (roll '() 0)))
+    (should= [0] (roll new-game 0)))
 
   (it "should add successful roll to a given game"
-    (should= [1] (roll '() 1)))
+    (should= [1] (roll new-game 1)))
 
   (it "should score gutter game as 0"
-    (should= 0 (score (roll-many 20 0))))
+    (should= 0 (score-game (roll-many 20 0))))
 
   (it "should score game of 1's as 20"
-    (should= 20 (score (roll-many 20 1)))))
+    (should= 20 (score-game (roll-many 20 1))))
+
+  #_(it "should score bonus for spare"
+      (should= 16 (score-game (-> new-game
+                                  (roll 5)
+                                  (roll 5)
+                                  (roll 3)
+                                  (concat (roll-many 17 0)))))))
--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -3,5 +3,15 @@
 (defn roll [rolls pins]
   (conj rolls pins))
 
-(defn score [rolls]
-  (reduce + rolls))
+(defn get-next-frame [rolls]
+  (next (next rolls)))
+
+(defn score-frame [rolls]
+  (+ (first rolls) (nth rolls 1)))
+
+(defn score-game [rolls]
+  (loop [rolls rolls
+         score 0]
+    (if (nil? (next rolls))
+      score
+      (recur (get-next-frame rolls) (+ score (score-frame rolls))))))
