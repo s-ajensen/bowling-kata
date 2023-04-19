7d5412a774f34b5cbe6c633afbc96cc3eb35e1b6
Alex Jensen <alex@endure.io>
2023-04-19 16:38:06 -0400

    Refactored scoring frame

--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -3,23 +3,29 @@
 (defn roll [rolls pins]
   (conj rolls pins))
 
-(defn get-next-frame [rolls]
-  (if (= 10 (first rolls))
-    (next rolls)
-    (next (next rolls))))
-
 (defn is-spare [rolls]
   (= 10 (+ (first rolls) (nth rolls 1))))
 
+(defn is-strike [rolls]
+  (= 10 (first rolls)))
+
+(defn get-next-frame [rolls]
+  (if (is-strike rolls)
+    (next rolls)
+    (next (next rolls))))
+
 (defn score-spare [rolls]
-  (+ (first rolls) (nth rolls 1)))
+  (+ 10 (nth rolls 2)))
+
+(defn score-strike [rolls]
+  (+ 10 (nth rolls 1 ) (nth rolls 2)))
 
 (defn score-frame [rolls]
-  (if (= 10 (first rolls))
-    (+ 10 (nth rolls 1 ) (nth rolls 2))
+  (if (is-strike rolls)
+    (score-strike rolls)
     (if (is-spare rolls)
-      (+ 10 (nth rolls 2))
-      (score-spare rolls))))
+      (score-spare rolls)
+      (+ (first rolls) (nth rolls 1)))))
 
 (defn score-game [rolls]
   (loop [rolls rolls
