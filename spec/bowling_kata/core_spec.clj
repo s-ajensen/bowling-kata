e09fde593a5b6655ab3d37d8ec46aaf2452a4cbb
Alex Jensen <alex@endure.io>
2023-04-24 11:55:46 -0400

    Completed kata

--- spec/bowling_kata/core_spec.clj
+++ spec/bowling_kata/core_spec.clj
@@ -2,6 +2,38 @@
   (:require [speclj.core :refer :all]
             [bowling-kata.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(def new-game [])
+
+(defn roll-many [rolls pins num-rolls]
+  (nth (iterate #(roll % pins) rolls) num-rolls))
+
+(describe "bowling kata tests"
+  (it "should add rolls to the game"
+    (should= [0] (roll new-game 0))
+    (should= [0 0] (roll-many new-game 0 2))
+    (should= [1 1 1] (roll-many new-game 1 3)))
+
+  (it "should score game of gutter balls as 0"
+    (should= 0 (score-game (roll-many new-game 0 20))))
+
+  (it "should score game of 1 pin rolls as 20"
+    (should= 20 (score-game (roll-many new-game 1 20))))
+
+  (it "should give bonus for rolling a spare"
+    (should= 16 (score-game (-> new-game
+                              (roll-many 5 2)
+                              (roll 3)
+                              (roll-many 0 17)))))
+
+  (it "should score game of spares as 150"
+    (should= 150 (score-game (roll-many new-game 5 21))))
+
+  (it "should give bonus for rolling a strike"
+    (should= 20 (score-game (-> new-game
+                              (roll 10)
+                              (roll 2)
+                              (roll 3)
+                              (roll-many 0 17)))))
+
+  (it "should score perfect game as 300"
+    (should= 300 (score-game (roll-many new-game 10 12)))))
--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,5 +1,34 @@
 (ns bowling-kata.core)
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(defn roll [rolls pins]
+  (conj rolls pins))
+
+(defn is-spare [rolls]
+  (= 10 (+ (first rolls) (nth rolls 1))))
+
+(defn is-strike [rolls]
+  (= 10 (first rolls)))
+
+(defn score-strike [rolls]
+  (if (nil? (nth rolls 2 nil))
+    0
+    (+ 10 (nth rolls 1) (nth rolls 2))))
+
+(defn progress-frame [rolls]
+  (if (is-strike rolls)
+    (next rolls)
+    (next (next rolls))))
+
+(defn score-frame [rolls]
+  (if (is-strike rolls)
+    (score-strike rolls)
+    (if (is-spare rolls)
+      (+ 10 (nth rolls 2))
+      (+ (first rolls) (nth rolls 1)))))
+
+(defn score-game [rolls]
+  (loop [rolls rolls
+         score 0]
+    (if (nil? (next rolls))
+      score
+      (recur (progress-frame rolls) (+ score (score-frame rolls))))))
